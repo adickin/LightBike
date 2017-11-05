962f73e73a24ac44263ebdef0e6f28f97ef1455c
adickin <adamdickin@gmail.com>
2017-11-05 15:42:17 -0700

    code to make light go around the wheel. mostly test code.

--- LightBikeMain/LightBike.cpp
+++ LightBikeMain/LightBike.cpp
@@ -3,12 +3,26 @@
 
 #include <FastLED.h>
 
-#define DATA_PIN 6
-#define NUM_LEDS 60
+//Should make a small class that does this for me.
+namespace TestLedStrip
+{
+   const int DATA_PIN = 6;
+   const int NUM_LEDS = 10;
+
+   CRGB leds[NUM_LEDS];
+}
+
+namespace CircularWheelLedStrip
+{
+   const int DATA_PIN = 8;
+   const int NUM_LEDS = 116;
+
+   CRGB leds[NUM_LEDS];
+}
+
 const unsigned int CHANGE_INTERVAL_MS = 300;
 
 unsigned int g_previousMiliseconds;
-CRGB g_leds[NUM_LEDS];
 
 
 bool AreCRGBsEqual(
@@ -30,30 +44,58 @@ void LightBike::Setup()
    g_previousMiliseconds = 0u;
 
 
-   FastLED.addLeds<NEOPIXEL, DATA_PIN>(g_leds, NUM_LEDS);
+   //FastLED.addLeds<NEOPIXEL, TestLedStrip::DATA_PIN>(TestLedStrip::leds, TestLedStrip::NUM_LEDS);
+
+   FastLED.addLeds<NEOPIXEL, CircularWheelLedStrip::DATA_PIN>(CircularWheelLedStrip::leds, CircularWheelLedStrip::NUM_LEDS);
+
+
+   for (int i = 0; i < CircularWheelLedStrip::NUM_LEDS; i++)
+   {
+      CircularWheelLedStrip::leds[i] = CRGB::Black;
+   }
+
+
 }
 
 void LightBike::Loop()
 {
-   unsigned int currentMilliseconds = millis();
-   if (!intervalReached(currentMilliseconds, g_previousMiliseconds, CHANGE_INTERVAL_MS))
-   {
-      return;
-   }
-   g_previousMiliseconds = currentMilliseconds;
+   //unsigned int currentMilliseconds = millis();
+   //if (!intervalReached(currentMilliseconds, g_previousMiliseconds, CHANGE_INTERVAL_MS))
+   //{
+   //   return;
+   //}
+   //g_previousMiliseconds = currentMilliseconds;
 
-   CRGB colorToUse = CRGB::White;
 
-   for (int i = 0; i < 10; i++)
+   for (int i = 0; i < CircularWheelLedStrip::NUM_LEDS; i++)
    {
-      if (AreCRGBsEqual(g_leds[i], CRGB::White))
+      if (AreCRGBsEqual(CircularWheelLedStrip::leds[i], CRGB::White))
       {
-         colorToUse = CRGB::Red;
+         CircularWheelLedStrip::leds[i] = CRGB::Black;
       }
+      else
+      {
+         CircularWheelLedStrip::leds[i] = CRGB::White;
+      }
+      
 
-      g_leds[i] = colorToUse;
+      //delay(100);
+      FastLED.show();
+
    }
 
-   FastLED.show();
+   //CRGB colorToUse = CRGB::White;
+
+   //for (int i = 0; i < TestLedStrip::NUM_LEDS; i++)
+   //{
+   //   if (AreCRGBsEqual(g_leds[i], CRGB::White))
+   //   {
+   //      colorToUse = CRGB::Red;
+   //   }
+
+   //   g_leds[i] = colorToUse;
+   //}
+
+   //FastLED.show();
 }
 
--- README.md
+++ README.md
@@ -6,18 +6,19 @@ This is a list of everything I ended up needing to finish the project
 
 |Part|Number|
 |---|---|
-| Ardunio Uno R3 Starter Kit | 1 |
-| 5M of WS2812B LED lights | 1 |
+| Arduino Uno R3 Starter Kit | 1 |
+| 5M of WS2812B LED lights, 60 lights/meter | 1 |
 | 5V 3A External DC power supply | 1 |
-| Digital Mulitmeter | 1 |
+| Digital Multimeter | 1 |
 | Red roll of 20 Gauge Wire | 1 |
 | Black roll of 20 Gauge Wire | 1 | 
 | Soldering Iron | 1 |
 | Wire Strippers | 1 |
 | Colored Electrical tape | 1 |
-| Zip Ties | 1 |
+| Zip Ties | 100 |
 | Old Bike | 1 |
 | Xacto Knife | 1 |
+| Poster board 22in x 28 in | 1 |
  
 
 ## Log
@@ -31,4 +32,18 @@ This is a list of everything I ended up needing to finish the project
 - Bought a digital multimeter as I needed to determine the +/- ends of the External power supply
 - Wired up the simple light flashing program with the external power supply.
   - Leds were much brighter
-- Setup Git to track my progress so I dont lose it again.
+- Setup Git to track my progress so I don't lose it again.
+
+### Oct 22, 2017
+- Cut a large piece of poster board to fit inside the wheel of the bike
+  - Doesn't fit perfectly but its close and good enough to build with
+- Cut a strip of lights to go around the circumference of the wheel, left an extra 2 leds just in case.(119 leds in all)
+  - Soldered wires onto that strip of leds so I can power them,
+    - I now really wish I would have bought 3 different colors of wire...
+- Hooked up the 119 led strip to the arduino board and was able to power them all!
+
+### Nov 5, 2017
+- Cut 6 strips of leds, 16 lights per strip
+- Attach large piece of poster board to the wheel in progress to provide a flat surface for light attachment
+- Remove 3 lights from the 119 led strip that circles the wheel
+  - Its now 116 lights.
